bbe7a532142ae5e75c1f56339e729f1c990fd69d
bepis <luqdek@gmail.com>
2021-01-12 10:24:50 +0000

    Split up the array by groups of 7 so that it's sorted into weeks

--- script.rb
+++ script.rb
@@ -1,5 +1,13 @@
 def stock_picker(arr)
-    
+    #weeks array
+    #take the passed array
+    #split it up by each 7 items
+    #take the array with the 7 times and place it in a week array
+    #do this for each group of 7
+    #do the check for each week
+    weeks = arr.each_slice(7).to_a
+
+
 end
 
 stock_picker([17,3,6,9,15,8,6,1,10])
