26fdcf051e4c9b733a91ba8e6420e594b6f955c5
bepis <luqdek@gmail.com>
2021-01-12 11:39:28 +0000

    commented out old unworking codde

--- script.rb
+++ script.rb
@@ -1,3 +1,4 @@
+require "pry"
 def stock_picker(arr)
     #weeks array
     #take the passed array
@@ -6,20 +7,29 @@ def stock_picker(arr)
     #do this for each group of 7
     #do the check for each week
     weeks = arr.each_slice(7).to_a
-    weeks.each do |item|
-        lowest = item.min
-        lowest_index = item.index(lowest)
-        highest = item.max
-        highest_index = item.index(highest)
-        while highest_index<lowest_index && item.length!=0
-            item_copy = item
-            item_copy.delete(highest)
-            highest = item_copy.max
-            highest_index = item_copy.index(highest)
-        end
-        puts "Best day to buy is " + lowest_index.to_s + " on week " + weeks.index(item).to_s
-        puts "Best day to sell is " + highest_index.to_s + " on week " + weeks.index(item).to_s
+    # weeks.each do |item|
+    #     lowest = item.min
+    #     lowest_index = item.index(lowest)
+    #     highest = item.max
+    #     highest_index = item.index(highest)
+    #     while highest_index<lowest_index && item.length!=0
+    #         binding.pry
+    #         item_copy = item
+    #         item_copy.delete(highest)
+    #         highest = item_copy.max
+    #         highest_index = item_copy.index(highest)
+    #     end
+
+    
+
+        # puts "Best day to buy is " + lowest_index.to_s + " on week " + weeks.index(item).to_s
+        # puts "Best day to sell is " + highest_index.to_s + " on week " + weeks.index(item).to_s
         
+    weeks.each do |week|
+        lowest = week.min
+        lowest_index = week.index(lowest)
+                
+
     end
 
 
